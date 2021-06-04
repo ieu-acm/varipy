5361576bd40f9d54c9d7b9cbf35a11302a010aab
Goodsea <kutsal_baran@hotmail.com>
2021-06-04 16:52:01 +0300

    Add docstring and check PEP8

--- src/train_utils.py
+++ src/train_utils.py
@@ -1,19 +1,29 @@
 """ Data training and validation tools """
 import torch
-from tqdm import tqdm   
+from tqdm import tqdm
 from torch.cuda.amp import autocast, GradScaler
 
+
 class TrainingManager:
-    """ PyTorch Training Manager 
+    """ PyTorch Training Manager
+
+    Args:
+        model: PyTorch model to be trained
+        loss_fn: Cost function
+        train_dloader: Training data loader
+        val_dloader: Validation data loader
+        device: PyTorch device that specify model training hardware: \
+            torch.device("cpu") or torch.device("cuda") available
     """
-    def __init__(self, model, loss_fn, optimizer, train_loader, val_loader, device):
+    def __init__(self, model, loss_fn, optimizer,
+                       train_dloader, val_dloader, device):
         self.__model = model
         self.__loss_fn = loss_fn
-        self.__optimizer = optimizer 
-        self.__train_loader = train_loader
-        self.__val_loader = val_loader
+        self.__optimizer = optimizer
+        self.__train_dloader = train_dloader
+        self.__val_dloader = val_dloader
         self.__device = device
-        
+
         self.__scaler = GradScaler()
 
     def train_epoch(self, epoch: int):
@@ -22,9 +32,8 @@ class TrainingManager:
         Args:
             epoch (int): Current epoch number
         """
-
         self.__model.train()
-        
+
         total_loss = 0
         total_samples = 0
 
@@ -38,17 +47,18 @@ class TrainingManager:
             with autocast():
                 _prediction = self.__model(_input)
                 loss = self.__loss_fn(_prediction, _output)
-            
-            total_loss += loss.item() 
+
+            total_loss += loss.item()
             total_samples += _input.size()[0]
-            
+
             self.__scaler.scale(loss).backward()
             self.__scaler.step(self.__optimizer)
             self.__scaler.update()
 
             pbar.set_description(f'epoch {epoch} loss: {loss.item():.4f}')
-        
-        pbar.set_description(f'epoch {epoch} average_loss: {total_loss/total_samples:.4f}')
+
+        pbar.set_description(f'epoch {epoch} \
+            average_loss: {total_loss/total_samples:.4f}')
 
     def validate(self, epoch):
         """ Validate a epoch
@@ -76,4 +86,5 @@ class TrainingManager:
 
             pbar.set_description(f'epoch {epoch} val_loss: {loss.item():.4f}')
 
-        pbar.set_description(f'epoch {epoch} average_val_loss: {total_loss/total_samples:.4f}')
+        pbar.set_description(f'epoch {epoch} \
+            average_val_loss: {total_loss/total_samples:.4f}')
