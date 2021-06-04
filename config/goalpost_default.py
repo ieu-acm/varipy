b71ed33321e124d1eb9663fadec2b58a06a8b0d6
Alican Akca <alicanakca_eternal@hotmail.com>
2021-06-04 18:22:50 +0300

    augmentation steps was added

--- config/goalpost_default.py
+++ config/goalpost_default.py
@@ -1,4 +1,5 @@
 """ Project configuration parameters """
+import imgaug
 
 class config:
     path = "data" # Relative to home directory of repository, 
@@ -8,9 +9,22 @@ class config:
     num_workers = 4
     val_ratio = 0.2
 
-    train_transforms = None
     valid_transforms = None
 
     weights_path = "weights"
     epochs = 50
     batch_size = 16
+
+    train_transforms = iaa.Sequential([
+        iaa.Crop(px=(1,16),keep_size=False),
+        iaa.Fliplr(0.5),
+        iaa.MotionBlur(),
+        iaa.FastSnowyLandscape(
+        lightness_threshold=[128, 200],
+        lightness_multiplier=(1.5, 3.5)),
+        iaa.Rain(speed=(0.1, 0.3)),
+        iaa.Snowflakes(flake_size=(0.1, 0.4), speed=(0.01, 0.05)),
+        iaa.Fog(),
+        iaa.imgcorruptlike.Brightness(severity=2),
+        iaa.ShearX((-20, 20))
+    ])
