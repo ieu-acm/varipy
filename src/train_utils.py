b43913abdda65ca23a6c6be40235b835d8f28d78
Goodsea <kutsal_baran@hotmail.com>
2021-06-19 19:01:04 +0300

    amp with choice

--- src/train_utils.py
+++ src/train_utils.py
@@ -5,7 +5,7 @@ from torch.cuda.amp import autocast, GradScaler
 
 
 class TrainingManager:
-    """ PyTorch Training Manager
+    """ PyTorch Training Manager with Mixed Precision Support
 
     Args:
         model: PyTorch model to be trained
@@ -16,15 +16,17 @@ class TrainingManager:
             torch.device("cpu") or torch.device("cuda") available
     """
     def __init__(self, model, loss_fn, optimizer,
-                       train_dloader, val_dloader, device):
+                       train_dloader, val_dloader, device, amp=False):
         self.__model = model
         self.__loss_fn = loss_fn
         self.__optimizer = optimizer
         self.__train_dloader = train_dloader
         self.__val_dloader = val_dloader
         self.__device = device
+        self.__amp = amp
 
-        self.__scaler = GradScaler()
+        if self.__amp:
+            self.__scaler = GradScaler()
 
     def train_epoch(self, epoch: int):
         """ Train a epoch
@@ -37,24 +39,31 @@ class TrainingManager:
         total_loss = 0
         total_samples = 0
 
-        pbar = tqdm(self.__train_loader, total=len(self.__train_loader))
+        pbar = tqdm(self.__train_dloader, total=len(self.__train_dloader))
         for (_input, _output) in pbar:
             self.__optimizer.zero_grad()
 
             _input = _input.to(self.__device).float()
             _output = _output.to(self.__device).float()
 
-            with autocast():
+            if self.__amp:
+                with autocast():
+                    _prediction = self.__model(_input)
+                    loss = self.__loss_fn(_prediction, _output)
+
+                self.__scaler.scale(loss).backward()
+                self.__scaler.step(self.__optimizer)
+                self.__scaler.update()
+
+            else:
                 _prediction = self.__model(_input)
                 loss = self.__loss_fn(_prediction, _output)
 
+                loss.backward()
+                self.__optimizer.step()
+
             total_loss += loss.item()
             total_samples += _input.size()[0]
-
-            self.__scaler.scale(loss).backward()
-            self.__scaler.step(self.__optimizer)
-            self.__scaler.update()
-
             pbar.set_description(f'epoch {epoch} loss: {loss.item():.4f}')
 
         pbar.set_description(f'epoch {epoch} \
@@ -72,7 +81,7 @@ class TrainingManager:
         total_loss = 0
         total_samples = 0
 
-        pbar = tqdm(self.__val_loader, total=len(self.__val_loader))
+        pbar = tqdm(self.__val_dloader, total=len(self.__val_dloader))
         for (_input, _output) in pbar:
             _input = _input.to(self.__device).float()
             _output = _output.to(self.__device).float()
